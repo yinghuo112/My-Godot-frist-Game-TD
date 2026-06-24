ce1c95c5a6360b3b7d7e17dbc1a79c3e846fdeb8
yinghuo112 <2945113765@qq.com>
2026-06-24 18:00:05 +0800

    auto backup 2026-06-24 18:00:04

--- UI/主题/设置界面/设置界面.gd
+++ UI/主题/设置界面/设置界面.gd
@@ -3,6 +3,16 @@ extends PanelContainer
 @onready var fs_btn: CheckButton = $主面板/HBoxContainer/CheckButton
 @onready var music_slider: HSlider = $主面板/音量盒/HSlider
 @onready var sfx_slider: HSlider = $主面板/音效盒/HSlider
+@onready var res_dropdown: OptionButton = $主面板/HBoxContainer/ResolutionDropdown
+
+var resolutions: Array[Vector2i] = [
+	Vector2i(1280, 720),
+	Vector2i(1366, 768),
+	Vector2i(1600, 900),
+	Vector2i(1920, 1080),
+	Vector2i(2560, 1440),
+	Vector2i(3840, 2160),
+]
 
 func _ready():
 	process_mode = PROCESS_MODE_WHEN_PAUSED
@@ -11,6 +21,8 @@ func _ready():
 	music_slider.value_changed.connect(_on_music_vol_changed)
 	sfx_slider.value_changed.connect(_on_sfx_vol_changed)
 	$主面板/ButtonHBox/CloseBtn2.pressed.connect(_on_close)
+	_populate_resolutions()
+	res_dropdown.item_selected.connect(_on_resolution_selected)
 	visible = false
 
 func open():
@@ -21,6 +33,23 @@ func close():
 	visible = false
 	get_tree().paused = false
 	AudioManager.play_music()
+
+func _populate_resolutions():
+	res_dropdown.clear()
+	var current_size = DisplayServer.window_get_size()
+	var select_idx = 0
+	for i in resolutions.size():
+		var r = resolutions[i]
+		res_dropdown.add_item("%d×%d" % [r.x, r.y])
+		if r == current_size:
+			select_idx = i
+	res_dropdown.select(select_idx)
+
+func _on_resolution_selected(idx: int):
+	if idx < 0 or idx >= resolutions.size():
+		return
+	var size = resolutions[idx]
+	DisplayServer.window_set_size(size)
 
 func _on_fullscreen_toggled(toggled_on: bool):
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if toggled_on else DisplayServer.WINDOW_MODE_WINDOWED)
